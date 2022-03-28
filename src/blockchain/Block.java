944efd59853f1748a2fb51f99583c1b7e468013e
Abeer Msameh <abeermosameh@gmail.com>
2022-03-28 21:02:24 +0300

    Java

---
+++ src/blockchain/Block.java
@@ -0,0 +1,68 @@
+package blockchain;
+
+import java.util.ArrayList;
+import java.util.Date;
+import com.google.gson.GsonBuilder;
+
+public class Block {
+	
+	public String hash;
+	public String previousHash; 
+	public String merkleRoot;
+	public ArrayList<Transaction> transactions = new ArrayList<Transaction>(); //our data will be a simple message.
+	public long timeStamp; //as number of milliseconds since 1/1/1970.
+	public int nonce;
+	
+	//Block Constructor.  
+	public Block(String previousHash ) {
+		this.previousHash = previousHash;
+		this.timeStamp = new Date().getTime();		
+		this.hash = calculateHash(); 
+	}
+	
+	//Calculate hash of block
+	public String calculateHash() {
+		String calculatedhash = StringUtil.applySha256( 
+				previousHash +
+				Long.toString(timeStamp) +
+				Integer.toString(nonce) + 
+				merkleRoot
+				);
+		return calculatedhash;
+	}
+	
+	//Increases nonce value until hash target is reached.
+	public void mineBlock(int difficulty) {
+		merkleRoot = StringUtil.getMerkleRoot(transactions);
+		String target = StringUtil.getDificultyString(difficulty); //Create a string with difficulty * "0" 
+		while(!hash.substring( 0, difficulty).equals(target)) {
+			nonce ++;
+			hash = calculateHash();
+		}
+		System.out.println("Block Mined!!! : " + hash);
+	}
+	
+	//Add transactions to this block
+	public boolean addTransaction(Transaction transaction) {
+		//process transaction and check if valid, unless block is genesis block then ignore.
+		if(transaction == null) return false;		
+		if((!"0".equals(previousHash))) {
+			if((transaction.processTransaction() != true)) {
+				System.out.println("Transaction failed to process. Discarded.");
+				return false;
+			}
+		}
+
+		transactions.add(transaction);
+		System.out.println("Transaction Successfully added to Block");
+		return true;
+	}
+
+	//print block in json format
+	public static String ExploreBlock(ArrayList<Block> blockchain) {
+		String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println(blockchainJson);	
+        return blockchainJson;
+	}
+	
+}
